8c5b08387cdf79d55360d6f782c1d3515032f743
Andre Carlucci <andrecarlucci@gmail.com>
2015-07-24 00:43:38 -0300

    init

---
+++ Hadoken/Light.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Hadoken {
+    public static class Light {
+        private static HttpClient _client = new HttpClient();
+
+        public static async Task Blink() {
+            await _client.GetAsync("http://192.168.10.253/cgi-bin/relay.cgi?on");
+            Console.WriteLine("On");
+            await Task.Delay(3000);
+            await _client.GetAsync("http://192.168.10.253/cgi-bin/relay.cgi?off");
+            Console.WriteLine("Off");
+        }
+    }
+}
---
+++ Hadoken/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Dear;
+using Dear.KeyboardControl;
+using SharpSenses;
+using SharpSenses.Gestures;
+
+namespace Hadoken {
+    class Program {
+        static void Main(string[] args) {
+            var win = new MrWindows();
+            var k = win.Keyboard;
+
+            Action hadouken = () => {
+                k.Press(VirtualKey.Down).Wait(20);
+                k.Press(VirtualKey.Right).Wait(20);
+                k.Release(VirtualKey.Down);
+                k.Press(VirtualKey.X).Wait(20);
+                k.Release(VirtualKey.Right);
+                k.Release(VirtualKey.X);
+                Console.WriteLine("HADOUKEN!!!!!!!!!!!!!");
+                Light.Blink();
+            };
+
+            var cam = Camera.Create();
+            cam.LeftHand.Visible += (sender, eventArgs) => {
+                Console.WriteLine("Ready");
+            };
+            var movement = Movement.Forward(cam.LeftHand, 8);
+            movement.Completed += () => {
+                hadouken.Invoke();
+            };
+            movement.Activate();
+
+            cam.Start();
+            Console.ReadLine();
+        }
+    }
+}
